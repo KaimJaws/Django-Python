52f613fa79f6eb178be286f6d41b7c5990ca5e87
Oscar González <xxmaestroyixx@gmail.com>
2026-02-26 15:44:19 -0400

    Se realizaron los siguientes cambios y adiciones:
    - Se actualizó la documentación en Guide.txt y se añadieron las secciones de Buenas Prácticas y Type Hinting.
    - Se modificó parte del código añadiendo el Type Hinting.
    - Se añadió un campo de descripción para el apartado de Mis Tareas.
    - Se modificó la lógica para filtrar entre tareas completadas y pendientes.

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+    title: str = models.CharField(max_length=100)
+    description: str = models.TextField(null=True, blank=True)
+    completed: bool = models.BooleanField(default=False)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from django.db.models import QuerySet
 from .models import Task
 
-def tasks_list(request):
-    tasks = Task.objects.all()
-    return render(request, 'myapp/home.html', {'tasks': tasks})
+def tasks_list(request: HttpRequest) -> HttpResponse:
+    pending_tasks: QuerySet[Task] = Task.objects.filter(completed=False)
+    completed_tasks: QuerySet[Task] = Task.objects.filter(completed=True)
 
-def create_task(request):
+    context = {
+        'pending_tasks': pending_tasks,
+        'completed_tasks': completed_tasks
+    }
+
+    return render(request, 'myapp/home.html', context)
+
+def create_task(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
-        title_task = request.POST.get('title')
+        title_task: str = request.POST.get('title')
+        description_task: str = request.POST.get('description', '')
         if title_task:
-            Task.objects.create(title=title_task)
+            Task.objects.create(title=title_task, description=description_task)
     return redirect('home')
 
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+def delete_task(request: HttpRequest, task_id: int) -> HttpResponse:
+    task: Task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect('home')
 
-def complete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+def complete_task(request: HttpRequest, task_id: int) -> HttpResponse:
+    task: Task = get_object_or_404(Task, id=task_id)
     task.completed = not task.completed
     task.save()
     return redirect('home')
